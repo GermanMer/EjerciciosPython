74d20470b87bbb9114dd1bddfc19a85953e0c3df
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-06-30 07:43:26 -0300

    Add files via upload

---
+++ PynativeExercise11.py
@@ -0,0 +1,15 @@
+#Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
+#For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+number=7536
+number=str(number)
+number=number[::-1]
+
+result=[]
+
+for i in number:
+  result.append(i)
+
+result=" ".join(result)
+
+print(result)
