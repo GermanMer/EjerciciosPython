63511bfc247555a0996eeadb62685b06645762db
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-07-07 11:22:16 -0300

    Add files via upload

---
+++ InputAndOutput01.py
@@ -0,0 +1,7 @@
+#Exercise 1: Accept numbers from a user
+#Write a program to accept two numbers from the user and calculate multiplication
+
+number1 = input('Please input the first number:')
+number2 = input('Please input the second number:')
+
+print('Multiplication is:', int(number1)*int(number2))
