3943d20d7bf52a1be5eea58173c47876b70bc6a7
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-07-11 19:54:58 -0300

    Add files via upload

---
+++ InputAndOutput02.py
@@ -0,0 +1,11 @@
+#Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+#Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+print("Name", "Is", "James", sep="**")
+
+print("Now, try with 3 words that you want")
+string1 = input("Enter first word:")
+string2 = input("Enter second word:")
+string3 = input("Enter third word:")
+
+print(string1, string2, string3, sep="**")
