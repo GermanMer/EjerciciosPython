9805a86a639593a7647de4a7a6617338aa10df2f
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-07-12 10:41:10 -0300

    Add files via upload

---
+++ InputAndOutput03.py
@@ -0,0 +1,10 @@
+#Exercise 3: Convert Decimal number to octal using print() output formatting
+#Given:vnum = 8
+#Expected Output: The octal number of decimal number 8 is 10
+
+given_num = 8
+print('The octal number of decimal number', given_num, 'is', '%o' % given_num)
+
+#'%o' % given_num, realiza la operación de formato.
+    #'%o', es una cadena de formato que especifica el formato para un número octal.
+    # el operdor % se utiliza para realizar la inserción del valor en la cadena de formato.
