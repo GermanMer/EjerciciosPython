4bc0c4886c69000da5fb089199032e8d59956b91
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-07-24 11:27:14 -0300

    Add files via upload

---
+++ Loop01.py
@@ -0,0 +1,17 @@
+#Exercise 1: Print First 10 natural numbers using while loop
+#Expected output:
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+
+contador = 0
+while contador < 10:
+    contador = contador + 1
+    print(contador)
