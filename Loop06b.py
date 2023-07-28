36c24512e58d2e2a75d80ae72ee79fbb6a89d36f
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-07-28 10:44:36 -0300

    Add files via upload

---
+++ Loop06b.py
@@ -0,0 +1,10 @@
+#Exercise 6: Count the total number of digits in a number
+#Write a program to count the total number of digits in a number using a while loop.
+
+#For example, the number is 75869, so the output should be 5.
+
+#This is an alternative way to get the result; it does not use a while loop, but it is much simpler.
+#Esta es una manera alternativa de obtener el resultado, no usa un blucle while pero es mucho mas simple.
+
+number = input('Enter a number:')
+print(len(number))
