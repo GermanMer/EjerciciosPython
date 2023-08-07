f080170a69e1ca604be307e11a26cb8df045dcae
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-07 11:07:39 -0300

    Add files via upload

---
+++ Loop13b.py
@@ -0,0 +1,18 @@
+#Exercise 13: Find the factorial of a given number
+#Write a program to use the loop to find the factorial of a given number.
+
+#The factorial (symbol: !) means to multiply all whole numbers from the chosen number down to 1.
+
+#For example: calculate the factorial of 5
+#5! = 5 × 4 × 3 × 2 × 1 = 120
+#Expected output:
+#120
+
+number = input('Enter a number:')
+
+factorial = 1
+
+for i in range(1, int(number) + 1):
+    factorial = factorial * i
+
+print('The factorial is', factorial)
