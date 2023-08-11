cbac7c0adf070b68bbd40467aca86869806e8368
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-11 10:43:29 -0300

    Add files via upload

---
+++ Function01.py
@@ -0,0 +1,11 @@
+#Exercise 1: Create a function in Python
+
+#Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def name_age (a, b):
+    print(a, b)
+
+name = input('Enter a name: ')
+age = input('Enter an age: ')
+
+name_age(name, age)
