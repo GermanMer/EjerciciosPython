94457d6ff22e1a57679dfeb9aeb08efe34fecc04
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-08-22 11:19:48 -0300

    Add files via upload

---
+++ Function06b.py
@@ -0,0 +1,23 @@
+#Exercise 6: Create a recursive function
+
+#Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
+
+#A recursive function is a function that calls itself again and again.
+
+#Expected Output: 55
+
+def recursive(x, y):
+    r = x + y
+    x = r
+    if y > 1:
+        recursive(x, y - 1)
+    else:
+        print(r)
+
+recursive(0, 10)
+
+print('Now you select the start and end:')
+start = input('Enter the start:')
+end = input('Enter the end:')
+
+recursive(int(start), int(end))
