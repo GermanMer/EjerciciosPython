72b1345d8da470230bc3caf540144bc3c49c80f9
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-10-23 11:17:17 -0300

    Add files via upload

---
+++ DataStructure08.py
@@ -0,0 +1,20 @@
+#Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+
+#Given:
+#roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+#sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+
+#Expected Outcome:
+#After removing unwanted elements from list [47, 69, 76, 97]
+
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+
+result_list = []
+
+for i in roll_number:
+    if i in sample_dict.values():
+        result_list.append(i)
+    else: continue
+
+print('After removing unwanted elements from list', result_list)
