49ce9e34f6e603b80de517eb7d9c8d7fba7680ce
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-10-31 14:36:37 -0300

    Add files via upload

---
+++ Dictionary01b.py
@@ -0,0 +1,19 @@
+#Exercise 1: Convert two lists into a dictionary
+
+#Below are the two lists. Write a Python program to convert them into a dictionary in a way that item from list1 is the key and item from list2 is the value
+
+#keys = ['Ten', 'Twenty', 'Thirty']
+#values = [10, 20, 30]
+
+#Expected output:
+#{'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+diccionario = dict()
+
+for i in range(len(keys)):
+    diccionario.update({keys[i]: values[i]})
+
+print(diccionario)
