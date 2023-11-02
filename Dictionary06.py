2cb088c0dfdb3c0862cf610edbb61507b83fa3f3
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-11-02 14:31:44 -0300

    Add files via upload

---
+++ Dictionary06.py
@@ -0,0 +1,29 @@
+#Exercise 6: Delete a list of keys from a dictionary
+
+#Given:
+#sample_dict = {
+#    "name": "Kelly",
+#    "age": 25,
+#    "salary": 8000,
+#    "city": "New york"
+#}
+
+# Keys to remove
+#keys = ["name", "salary"]
+
+#Expected output:
+#{'city': 'New york', 'age': 25}
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+keys_to_remove = ["name", "salary"]
+
+for i in keys_to_remove:
+    sample_dict.pop(i)
+
+print(sample_dict)
