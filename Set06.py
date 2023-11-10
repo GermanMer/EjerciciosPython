7c6943397b96189462812871bd2d14b9ec835c3b
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-11-10 00:18:31 -0300

    Add files via upload

---
+++ Set06.py
@@ -0,0 +1,15 @@
+#Exercise 6: Return a set of elements present in Set A or B, but not both
+
+#Given:
+#set1 = {10, 20, 30, 40, 50}
+#set2 = {30, 40, 50, 60, 70}
+
+#Expected output:
+#{20, 70, 10, 60}
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+res_set = set1.symmetric_difference(set2)
+
+print(res_set)
