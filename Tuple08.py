177f5db895dfff764680625f3de56fb568c76d75
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-11-16 17:42:11 -0300

    Add files via upload

---
+++ Tuple08.py
@@ -0,0 +1,11 @@
+#Exercise 8: Sort a tuple of tuples by 2nd item
+
+#Given:
+#tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
+
+#Expected output:
+#(('c', 11), ('a', 23), ('d', 29), ('b', 37))
+
+tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
+
+print(sorted(tuple1, key=lambda x: x[1]))
