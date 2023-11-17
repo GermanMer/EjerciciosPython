98327dd4330479493720c2210546819b22077007
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-11-17 19:22:14 -0300

    Add files via upload

---
+++ Tuple10.py
@@ -0,0 +1,13 @@
+#Exercise 10: Check if all items in the tuple are the same
+
+#Given:
+#tuple1 = (45, 45, 45, 45)
+
+#Expected output:
+#True
+
+tuple1 = (45, 45, 45, 45)
+
+a, b, c, d = tuple1
+
+print(a == b == c == d)
