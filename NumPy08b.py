3a9e31d9121fdd87cd2b5db0c00f6adf80f85c9f
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-12-29 08:45:10 -0300

    Add files via upload

---
+++ NumPy08b.py
@@ -0,0 +1,27 @@
+#Exercise 8: Print max from axis 0 and min from axis 1 from the following 2-D array.
+
+#sampleArray = numpy.array([[34,43,73],[82,22,12],[53,94,66]])
+
+#Expected Output:
+#Printing Original array
+#[[34 43 73]
+# [82 22 12]
+# [53 94 66]]
+#Printing amin Of Axis 1
+#[34 12 53]
+#Printing amax Of Axis 0
+#[82 94 73]
+
+import numpy
+
+print("Printing Original array")
+sampleArray = numpy.array([[34,43,73],[82,22,12],[53,94,66]])
+print (sampleArray)
+
+minOfAxisOne = numpy.amin(sampleArray, 1)
+print("Printing amin Of Axis 1")
+print(minOfAxisOne)
+
+maxOfAxisOne = numpy.amax(sampleArray, 0)
+print("Printing amax Of Axis 0")
+print(maxOfAxisOne)
