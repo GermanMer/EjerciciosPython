55dcf194b6d1da247aeef57f10352e5fdc67458a
German Mercader <121909886+GermanMer@users.noreply.github.com>
2024-02-22 17:17:31 -0300

    Add files via upload

---
+++ Random04.py
@@ -0,0 +1,11 @@
+#Exercise 4: Pick a random character from a given String
+
+#Python random.choice() function to select a random item from a String, List, Set, etc.
+
+import random
+
+given_string = 'Exercise 4'
+
+random_character = random.choice(given_string)
+
+print(random_character)
