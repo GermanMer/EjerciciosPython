66e57a6de2dba189e1da850a2e3fca6a21234c23
German Mercader <121909886+GermanMer@users.noreply.github.com>
2024-03-07 22:54:16 -0300

    Add files via upload

---
+++ Random07.py
@@ -0,0 +1,9 @@
+#Exercise 7: Calculate multiplication of two random float numbers
+    #First random float number must be between 0.1 and 1
+    #Second random float number must be between 9.5 and 99.5
+
+import random
+a = random.random()
+b = random.uniform(9.5, 99.5)
+result = a*b
+print(result)
