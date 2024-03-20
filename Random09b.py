a2b48e0862b394f59d1c06d9a6de9bfde0496fe1
German Mercader <121909886+GermanMer@users.noreply.github.com>
2024-03-20 13:45:07 -0300

    Add files via upload

---
+++ Random09b.py
@@ -0,0 +1,9 @@
+#Exercise 9: Roll dice in such a way that every time you get the same number
+#Dice has 6 numbers (from 1 to 6). Roll dice in such a way that every time you must get the same output number. do this 5 times.
+
+import random
+
+dice = [1, 2, 3, 4, 5, 6]
+for i in range(5):
+    random.seed(2)
+    print(random.choice(dice))
